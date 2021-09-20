e473c38e45112cdf0103f52074361aaa3b64eca0
nestor-almanza <nuaa8583@gmail.com>
2021-09-19 20:56:41 -0500

    cross en controller

--- hospital/src/main/java/mx/com/personal/sgbd/hospital/controller/EspecialidadController.java
+++ hospital/src/main/java/mx/com/personal/sgbd/hospital/controller/EspecialidadController.java
@@ -6,12 +6,14 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import mx.com.personal.sgbd.hospital.model.Especialidad;
 import mx.com.personal.sgbd.hospital.service.IEspecialidadService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class EspecialidadController {
 	
